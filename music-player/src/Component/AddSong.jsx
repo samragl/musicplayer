d7baba5d093590761b96fb7f9967ce9861a90a93
samragl <lsamrag051@gmail.com>
2024-06-25 16:08:35 +0545

    ok

---
+++ music-player/src/Component/AddSong.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddSong = () => {
+  return (
+    <div>
+      <p>AddList</p>
+    </div>
+  );
+};
+
+export default AddSong;
---
+++ music-player/src/Component/SongList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SongList = () => {
+  return (
+    <div>
+      <p>Song list</p>
+    </div>
+  );
+};
+
+export default SongList;
